2e8a37759cebc4abbda80192a836d816dc14cbef
drako9159 <chochito9159@gmail.com>
2022-08-31 18:48:17 -0500

    add cors

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,4 +1,5 @@
 import { Formik, Form } from "formik";
+import { createTaskRequest } from "../api/Tasks.api";
 
 export default function TaskForm() {
   return (
@@ -8,25 +9,38 @@ export default function TaskForm() {
           title: "",
           description: "",
         }}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          try {
+            const response = await createTaskRequest(values);
+            console.log(response);
+            actions.resetForm();
+          } catch (error) {
+            console.error(error);
+          }
+        }}
       >
-        {({ handleChange }) => (
-          <Form>
+        {({ handleChange, handleSubmit, values, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
             <label>Title</label>
             <input
               type="text"
               name="title"
               placeholder="Write a title"
-              onChange={{ handleChange }}
+              onChange={handleChange}
+              value={values.title}
             />
-
             <label>Description</label>
             <textarea
               name="description"
               rows="3"
               placeholder="Write a description"
-              onChange={{ handleChange }}
+              onChange={handleChange}
+              value={values.description}
             />
-            <button>Save</button>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </button>
           </Form>
         )}
       </Formik>
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export default function TaskPage() {
+  useEffect(() => {
+    console.log("TaskPage");
+  }, []);
+
   return (
     <div>
       <h1>Task Page</h1>
